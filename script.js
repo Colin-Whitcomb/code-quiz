7197651188cd0a0d6ceae49b15f95222715836fe
Colin Whitcomb <colinsw@berkeley.edu>
2020-06-20 17:34:59 -0700

    Added basic pseudocode structure.

---
+++ script.js
@@ -0,0 +1,21 @@
+// Timer:
+//    1) Count down from beginning
+//    2) Decrease if wrong answer 
+//    3) Display on screen 
+
+// Start button - click event 
+
+// View high school - click event 
+
+// For questions:
+//      1) Choose answer - click event 
+//      2) If correct - display "Correct!"
+//      3) If incorrect - display "Wrong!"
+
+// All done! 
+//     1) Enter Initials
+//      2) Submit button
+
+// High Scores: 
+//      1) Show IF clicked submit button OR by clicking "View Highschore"
+//      2) "View High Score" needs to be visible on screen. 
