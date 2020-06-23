308808b8b657e8ab1e109333b9b399a576244638
Colin Whitcomb <colinsw@berkeley.edu>
2020-06-22 19:29:22 -0700

    Stored score and initials in localStorage.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ https://www.youtube.com/watch?v=riDzcEQbX6k
  https://zellwk.com/blog/looping-through-js-objects/
 
  https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+ https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent 
--- script.js
+++ script.js
@@ -7,34 +7,15 @@ var openingHeaderEl = document.getElementById("welcome-div")
 var counterNum = document.getElementById("counter-num")
 // var buttonA = document.getElementById("button-a")
 var nextButton = document.getElementById("next-btn")
+var corrWrongEl = document.getElementById("correct-wrong")
+var submitBtn = document.getElementById("submitButton")
+var input = document.getElementById("input")
 
 var questionIndex;
 var score;
+var correct;
+var timerGlobal;
 
-// array of questions and their corresponding options and answer. 
-let questionsArr = [
-    {
-    question: "What is 4+8?",
-    options: ["8", "6", "12", "14",],
-    answer: "12",
-},{
-    question: "What is 4+4?",
-    options: ["8", "4", "44", "19", ],
-    answer: "8",
-},{
-    question: "What is the capital of New York?",
-    options: ["NYC", "Buffalo", "Albany", "Rochester",],
-    answer: "Albany",
-// },{
-//     question: "What is the name of Jeromes' cat?",
-//     options: ["Manuel", "Asher", "Farley", "Garfield", ],
-//     answer: "Farley",
-// },{
-//     question: "What is 4+4?",
-//     options: ["7", "4", "44", "19", ],
-//     answer: "8",
-}
-]
 // function to strt game - called by clicking Start
 function startGame() {
     // check to see if the function has been called
@@ -52,7 +33,7 @@ function startGame() {
     // set timer to max time
     questionIndex = 0;
     score = 0;
-    setInterval(updateTimer, 1000);
+    timerGlobal= setInterval(updateTimer, 1000);
     showQuestion();
 
 }
@@ -61,18 +42,16 @@ function startGame() {
 function showQuestion() {
     // writes the question line 
     questionEl.textContent = questionsArr[questionIndex].question;
+    // writes the answer text 
+    var answers = questionsArr[questionIndex].options;
     // for every answer on the question array...
-    // questionArr[questionIndex].answers.forEach( ()=> {
-        // // creates button element
-        // var button = document.createElement("button");
-        // writes the answer text 
-        var answers = questionsArr[questionIndex].options;
-        for (var i=0; i < answers.length; i++) {
+    for (var i = 0; i < answers.length; i++) {
+        // write that text content 
         answerButtonsEl.children[i].textContent = answers[i];
+        // listen for a click, then run selectAnswer
         answerButtonsEl.children[i].addEventListener("click", selectAnswer);
-        }
     }
-
+}
 
 //  function once answer is selected
 function selectAnswer(event) {
@@ -81,41 +60,114 @@ function selectAnswer(event) {
     // setting variable for which button is clicked
     var selectedButton = event.target.innerText;
     console.log(selectedButton);
+
     // determines whether or not the selected button is correct
     var correct = questionsArr[questionIndex].answer;
 
-    if (selectedButton = correct) {
+    if (selectedButton === correct) {
         score++; // make sure only one right answer per attempt
-        console.log(score);
-        // document.textContent = "Correct!"
-        // add text to say "Correct"
+        console.log("This is the score" + score);
+        // document.textContent = displays "Correct!"
+        corrWrongEl.classList.remove("hide");
+        corrWrongEl.children[0].textContent = "Correct!";
+
     } else {
-        // document.textContent = "Wrong!"
+        // changes text from "Correct!" to "Wrong!"
+        corrWrongEl.children[0].textContent = "Wrong!";
+        // removes hide element to make sure "Wrong is displayed"
+        corrWrongEl.classList.remove("hide")
+        // need more efficient way of doing this. 
+        time--;
+        time--;
     }
+
     nextButton.classList.remove("hide");
 }
 
-function nextQuestion () {
-questionIndex++;
-nextButton.classList.add("hide");
-showQuestion();
-// add if (last question) - go to submit initials page. 
-}
+function nextQuestion() {
+    // adds index to draw next question in array
+    questionIndex++;
+    console.log("This is the question index:" + questionIndex);
+    // hides the next button
+    nextButton.classList.add("hide");
+    //hides the "Correct!"/"Wrong!" text
+    corrWrongEl.classList.add("hide");
+    showQuestion();
+    // if you answer all questions - end game 
+    // if (questionIndex = 5) {
+    //     endGame ();
+    // }
 
+}
 // // total seconds in game
-let time = 45;
+let time = 10;
 
 //Timer function 
-
-   function updateTimer() {
-       console.log("Time has begun");
+function updateTimer() {
     // let seconds =;
-
+    time--;
     counterNum.innerHTML = time;
-       time--;
+    // if time runs out end game - needs work. 
+    if (time <= 0) {
+        endGame();
+    }
+}
 
-   }
+function endGame() {
+    clearInterval(timerGlobal);
+    questionContainerEl.classList.add("hide");
+    // nextButton.classList.add("hide");
+    // changes text from "Correct!" to "Wrong!"
+    corrWrongEl.children[0].textContent = "You got " + score + " out of 10 points!";
 
+}
+
+function createHighScoreList () {
+var dataCollected = JSON.parse(window.localStorage.getItem("grabScores")) || [];
+// dataCollected.sort descending order 
+dataCollected.forEach(element => {
+// parse JSON - returned as object {score: initials}
+});
+}
+
+function pushHighScore () {
+var initials = input.value.trim();
+if (initials !== "") {
+    var grabScores = JSON.parse(window.localStorage.getItem("getscores")) || [] ;
+    var newScore = {
+        heldScore: score,
+        heldInit: initials
+    };
+    grabScores.push(newScore);
+    window.localStorage.setItem("grabScores", JSON.stringify(grabScores));
+} 
+}
+
+
+// array of questions and their corresponding options and answer. 
+let questionsArr = [{
+    question: "What is the capital of South Carolina?",
+    options: ["Raleigh", "Myrtle Beach", "Charleston", "Columbia", ],
+    answer: "Columbia",
+}, {
+    question: "What is the capital of California?",
+    options: ["Sacramento", "Los Angeles", "San Francisco", "San Diego", ],
+    answer: "Sacramento",
+}, {
+    question: "What is the capital of New York?",
+    options: ["NYC", "Buffalo", "Albany", "Rochester", ],
+    answer: "Albany",
+}, {
+    question: "What is the capital of Florida?",
+    options: ["Tampa", "Orlando", "Miami", "Tallahassee", ],
+    answer: "Tallahassee",
+}, {
+    question: "What is the capital of Oregon?",
+    options: ["Portland", "Salem", "Oregon City", "Eugene", ],
+    answer: "Salem",
+}]
+
+submitBtn.addEventListener("click", pushHighScore);
 startButtonEl.addEventListener("click", startGame);
 nextButton.addEventListener("click", nextQuestion);
 
@@ -126,3 +178,14 @@ nextButton.addEventListener("click", nextQuestion);
 // High Scores: 
 //      1) Show IF clicked submit button OR by clicking "View Highschore"
 //      2) "View High Score" needs to be visible on screen
+
+// Questions: 
+// 1 - endgame function isn't working
+// when done with questions
+// when out of time
+// 2 - add view score page
+// 3 - only allow 1 answer per question
+// 4 - layout (grid instead of stack); moving up and down
+
+
+
