bc6b9b87a5c83dbcb278d94ce2c4eafd48e9156b
Colin Whitcomb <colinsw@berkeley.edu>
2020-06-23 22:31:46 -0700

    Trials through close HS button.

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var input = document.getElementById("input")
 var endGameForm = document.getElementById("endGameForm")
 var hsArea = document.getElementById("hsArea")
 var hsBtn = document.getElementById("view-highscore")
+var reStart = document.getElementById("reStart")
+var closeHs = document.getElementById("closeHS")
 // ================================================= //  
 
 // global variables 
@@ -35,6 +37,7 @@ function updateTimer() {
     }
 }
 
+
 // function to start game - called by clicking Start
 function startGame() {
     openingHeaderEl.style.display = "none";
@@ -45,12 +48,9 @@ function startGame() {
     score = 0;
     timerGlobal = setInterval(updateTimer, 1000);
     showQuestion();
-    // localStorage.setItem('my cat', 'Farley');
-    // var Farley = getItem('my cat');
+
 }
 
-// localStorage.setItem('my cat', 'Farley');
-// var Farley = getItem('my cat');
 // function that shows hidden questions and corresponding answers
 function showQuestion() {
     questionEl.textContent = questionsArr[questionIndex].question;
@@ -65,7 +65,6 @@ function showQuestion() {
 //  function once answer is selected
 function selectAnswer(event) {
     var selectedButton = event.target.innerText;
-    console.log(selectedButton);
     // determines whether or not the selected button is correct
     var correct = questionsArr[questionIndex].answer;
 
@@ -96,10 +95,12 @@ function nextQuestion() {
 }
 
 function endGame() {
+    console.log("game has ended");
     clearInterval(timerGlobal);
     questionContainerEl.classList.add("hide");
     nextButton.classList.add("hide");
     endGameForm.classList.remove("hide");
+    hsArea.classList.remove("hide");
     pushData();
     endGameForm.children[0].textContent = "You got " + score + " point(s)!";
 
@@ -107,27 +108,32 @@ function endGame() {
 
 //function to push to local storage 
 function pushData() {
+    printScores();
+    console.log("push has begun");
     var initials = input.value.trim();
     //putting score and initials in object
     if (initials !== "") {
         // check to see if there are any saved high scores in localStorage
+        
         var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
         var newScore = {
             score: score,
             initials: initials,
+            
         };
 
         // adding new player in to high score list
         highScores.push(newScore);
-        localStorage.setItem("highScores", JSON.stringify(newScore));
+        window.localStorage.setItem("highScores", JSON.stringify(highScores));
         //redirect to where I want to keep high scores. 
-        printScores();
-    }
+        // printScores();
+    } 
 }
 
 // taking info from local storage and display it 
 function printScores() {
     var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
+    console.log("Print has been initiated");
     // parse from JSON
     highScores.sort(function (a, b) {
         return b.score - a.score
@@ -141,15 +147,31 @@ function printScores() {
 
         var winners = document.getElementById("hsArea"); 
         winners.appendChild(player);
+        // showScores();
+        hsArea.classList.remove("hide");
     });
 
-// function clearScores() {
-//     window.localStorage.removeItem("highScores");
-//     window.location.reload();
-// }
-
-printScores();
+function clearScores() {
+    window.localStorage.removeItem("highScores");
+    window.location.reload();
 }
+
+document.getElementById("clear").onclick = clearScores;
+// printScores();
+};
+
+function showScores () {
+    hsArea.classList.remove('hide');
+}
+function reStart () {
+    window.location.reload();
+}
+
+function closeHs() {
+    hsArea.classList.add("hide"); 
+   
+}
+
 // array of questions and their corresponding options and answer. 
 let questionsArr = [{
         question: "What is the capital of South Carolina?",
@@ -175,7 +197,8 @@ let questionsArr = [{
     }
 ]
 
-// NEED a clear button!! 
-// document.getElementById("clear").onclick = clearScores;
+// closeHs.addEventListener("click", closeHS);
+// reStart.addEventListener("click", reStart);
+hsBtn.addEventListener("click", showScores);
 submitBtn.addEventListener("click", pushData);
 startButtonEl.addEventListener("click", startGame);
